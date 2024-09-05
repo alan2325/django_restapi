269d45d7d0e669b144ee5308c9ed7128cd5bcfcc
alan2325 <alalkumar06@gmail.com>
2024-09-05 11:28:35 +0530

    class

--- project/app/urls.py
+++ project/app/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('fun',views.fun1),
     path('fun2',views.fun2),
     path('fun3',views.fun3),
+    path('fun4',views.fun4),
+    path('fun5/<int:d>',views.fun5),
+    path('fun6',views.fun6.as_view()),
+    path('fun7/<int:d>',views.fun7.as_view()),
 ]
 
--- project/app/views.py
+++ project/app/views.py
@@ -4,6 +4,10 @@ from .models import*
 from .serializers import*
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.   
 def dict(req):
@@ -61,3 +65,83 @@ def fun3(req,d):
     elif req.method=='DELETE':
         demo.delete()
         return HttpResponse('deleted')
+
+
+@api_view(['GET','POST'])
+def fun4(req):
+    if req.method=='GET':
+        d=std.objects.all()
+        s=model_serializers(d,many=True)
+        return Response(s.data)
+    
+    elif req.method=='POST':
+        s=model_serializers(data=req.data)
+        if s.is_valid():
+            s.save()
+            return JsonResponse(s.data,status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse(s.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+@api_view(['GET','PUT','DELETE'])
+def fun5(req,d):
+    try:
+        demo=std.objects.get(pk=d)
+    except:
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    if req.method=='GET':
+        s=model_serializers(demo)
+        return Response(s.data)
+    elif req.method=='PUT':
+        s=model_serializers(demo,data=req.data)
+        if s.is_valid():
+            s.save()
+            return Response(s.data)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    elif req.method=='DELETE':
+        demo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+class fun6(APIView):
+    def get(self,req):
+        demo=std.objects.all()
+        s=model_serializers(demo,many=True)
+        return Response(s.data)
+    def post(self,req):
+        s= model_serializers(data=req.data)
+        if s.is_valid():
+            s.save()
+            return JsonResponse(s.data, status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse(s.errors ,status=status.HTTP_400_BAD_REQUEST)
+        
+class fun7(APIView):
+    def get(self,req,d):
+        try:
+            demo=std.objects.get(pk=d)
+            s=model_serializers(demo)
+            return Response(s.data)
+        except std.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+    def put(self,req,d):
+        try:
+            demo=std.objects.get(pk=d)
+            s=model_serializers(demo,data=req.data)
+            if s.is_valid():
+                s.save()
+                return Response(s.data)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        except std.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)    
+    def delete(self,req,d):
+        try:
+            demo=std.objects.get(pk=d)
+            demo.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except std.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    
